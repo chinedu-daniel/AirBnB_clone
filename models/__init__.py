a35267f2f01a7cae59361510e5ef746e727adc7d
Nosenti <inno100os@gmail.com>
2024-03-25 00:14:23 +0200

    debug base model

--- models/__init__.py
+++ models/__init__.py
@@ -5,11 +5,6 @@ Create unique storage instance
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
-from .amenity import Amenity
-from .city import City
-from .place import Place
-from .review import Review
-from state import State
 
 storage = FileStorage()
 storage.reload()
